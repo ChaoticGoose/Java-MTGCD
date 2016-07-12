5061fd2d55281ac169812761cddb7998a53b18b3
Shea Gosnell <gosnell_shea@hotmail.co.nz>
2016-07-12 14:16:17 +1200

    Re-arranged file structure

--- java/MTGCD.java
+++ java/MTGCD.java
@@ -368,35 +368,3 @@ class MTGCD
 		}
 	}
 }
-
-//private class to hold information about a card, makes passing information cleaner
-class Card
-{
-	private int CN;
-	private String SET;
-	private boolean Foil;
-	
-	public Card(int CollectorsNumber, String SetCode, boolean foil)
-	{
-		CN = CollectorsNumber;
-		SET = SetCode;
-		Foil = foil;
-	}
-	
-	public int getCN(){return CN;}
-	public String getSetCode(){return SET;}
-	public boolean getFoil(){return Foil;}
-	
-	public String getSQLFindString()
-	{
-		return 
-			" where Collector_Number = " +
-			CN + " AND Set_Code = '" + 
-			SET + "' AND Foil = " + 
-			(Foil ? "1":"0");
-	}
-	public String getFormatString(int Quantity)
-	{
-		return SET + "\t" + CN + "\t" + Quantity + "\t" + (Foil ? "Yes":"No");
-	}
-} 
